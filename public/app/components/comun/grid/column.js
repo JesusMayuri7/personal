1b59dc3315869be46c993b4e07abf54f2cccb5af
JesusMayuri7 <jesus.mayuri.cuellar@gmail.com>
2016-10-21 11:03:32 -0500

    'eliminando'

--- public/app/components/comun/grid/column.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Column = (function () {
-    function Column(columna, valor) {
-        this.columna = columna;
-        this.valor = valor;
-    }
-    return Column;
-}());
-exports.Column = Column;
--- public/app/components/rutas/app.rutas.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-var personal_component_1 = require('../../components/personal/personal.component');
-var menu_component_1 = require('../../components/menu/menu.component');
-var appRoutes = [
-    { path: 'Menu', component: menu_component_1.MenuComponent },
-    { path: '', component: personal_component_1.PersonalComponent }
-];
-exports.routing = router_1.RouterModule.forRoot(appRoutes);
